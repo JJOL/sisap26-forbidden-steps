94ba2dc753594a5e895e50a023ab7971c297aa64
Juan Jo <jjoulk@gmail.com>
2026-06-18 14:03:39 +0200

    change to use blocked_inverted algo

--- dry_run.sh
+++ dry_run.sh
@@ -1,6 +1,6 @@
 tira-cli code-submission \
     --path . \
-    --command '/app/build/main.exe --inputFolder $inputDataset --outputFolder $outputDir --strategy inverted --kTop 30 --task task3 --dataset fiqa-dev  --threads 1' \
+    --command '/app/build/main.exe --inputFolder $inputDataset --outputFolder $outputDir --strategy blocked-inverted --kTop 30 --task task3 --dataset fiqa-dev  --threads -1' \
     --task sisap-2026 \
     --dataset task-3-spot-check-20260529-training \
     --dry-run
